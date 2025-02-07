f384ce0fd207cf726c2b2b055b78fe81977ed6a9
kude7113 <125797737+kude7113@users.noreply.github.com>
2025-02-07 19:50:51 +0300

    done pwa

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import  App   from "./App.tsx"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {registerSW} from "virtual:pwa-register";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-)
+);
+
+if ('serviceWorker' in navigator) {
+    registerSW();
+}
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,8 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Tile Notes",
+        short_name: "Tile Notes",
+        start_url: "/RIP_frontend/",
+        scope: "/RIP_frontend/",
+        display: "standalone",
+        background_color: "#fdfdfd",
+        theme_color: "#db4938",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            "src": "/RIP_frontend/logo192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "/RIP_frontend/logo512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          }
+        ],
+        screenshots: [
+          {
+            "src": "/RIP_frontend/scr1.png", // <-- Исправленный путь
+            "sizes": "1262x768",
+            "type": "image/png",
+            "form_factor": "wide"
+          },
+          {
+            "src": "/RIP_frontend/scr2.png",
+            "sizes": "720x1280",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
+  ],
   base: "/RIP_frontend",
   server: {
     port: 3000,
